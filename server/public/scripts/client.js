0d0c346ff084f1cdb85f2ecd81958c1d6b6d787b
Luke Schlangen <lukeschlangen@gmail.com>
2016-11-29 07:34:40 -0600

    Adding angular snippet

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,6 @@ var app = angular.module('app', []);
 
 app.controller('firstController', function() {
   console.log('firstController up and running');
+  var self = this;
+  self.message = "Welcome!"
 });
